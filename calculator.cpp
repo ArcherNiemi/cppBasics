04f670515e88fd7096160f18c7dd3752ead77644
Archer N <kingdedede2019@gmail.com>
2026-05-28 16:56:13 -0500

    Added exit

--- calculator.cpp
+++ calculator.cpp
@@ -3,43 +3,52 @@
 int main(){
     std::string input;
 
-    std::cout << "Enter Basic Formula: ";
-    std::getline(std::cin, input);
+    bool running = true;
 
-    int count = 0;
+    while(running){
+        std::cout << "Enter Basic Formula: ";
+        std::getline(std::cin, input);
 
-    std::string inputPart = "";
-    for(int i = 0; i < size(input); i++){
-        if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/'){
+        if(input == "exit"){
+            running = false;
             break;
         }
-        inputPart.push_back(input[i]);
-        count++;
+
+        int count = 0;
+
+        std::string inputPart = "";
+        for(int i = 0; i < size(input); i++){
+            if(input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/'){
+                break;
+            }
+            inputPart.push_back(input[i]);
+            count++;
+        }
+
+        double num1 = std::stoi(inputPart);
+        inputPart = "";
+
+        char operation = input[count];
+
+        for(int i = count+1; i < size(input); i++){
+            inputPart.push_back(input[i]);
+        }
+
+        double num2 = std::stoi(inputPart);
+
+        double output;
+        if(operation == '+'){
+            output = num1 + num2;
+        } else if(operation == '-'){
+            output = num1 - num2;
+        } else if(operation == '*'){
+            output = num1 * num2;
+        }else if(operation == '/'){
+            output = num1 / num2;
+        }
+
+        std::cout << output << '\n';
     }
-
-    double num1 = std::stoi(inputPart);
-    inputPart = "";
-
-    char operation = input[count];
-
-    for(int i = count+1; i < size(input); i++){
-        inputPart.push_back(input[i]);
-    }
-
-    double num2 = std::stoi(inputPart);
-
-    double output;
-    if(operation == '+'){
-        output = num1 + num2;
-    } else if(operation == '-'){
-        output = num1 - num2;
-    } else if(operation == '*'){
-        output = num1 * num2;
-    }else if(operation == '/'){
-        output = num1 / num2;
-    }
-
-    std::cout << output;
 
     return 0;
 }
